77e25924d127fbcc9070cb68709eb3fe9d9c26c9
Danil Klusov <djshplis@gmail.com>
2022-11-13 14:27:56 +0500

    fix cafe request

---
+++ Foodcourt.Data/Api/Request/CafeCreateRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace Foodcourt.Data.Api.Request;
+
+public class CafeCreateRequest
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Adress { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public string CertifyingDocument { get; set; }
+    public string PersonalAccount { get; set; }
+}
