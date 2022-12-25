64d3762e3afff628676d4a4e7fb7765011affc1f
Klusov Danil <klusov@skbkontur.ru>
2022-12-26 02:43:14 +0500

    add resp

---
+++ Foodcourt.Data/Api/Response/FullApplicationEntity.cs
@@ -0,0 +1,16 @@
+﻿using Foodcourt.Data.Api.Entities.Cafes;
+
+namespace Foodcourt.Data.Api.Response;
+public class FullApplicationEntity
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public DateTime CreationTime { get; set; }
+    public string Address { get; set; }
+    public string Response { get; set; }
+    public CafeStatus Status { get; set; }
+    public string CertifyingDocument { get; set; }
+    public string PersonalAccount { get; set; }
+    public string Rating { get; set; }
+    public string Description { get; set; }
+}
